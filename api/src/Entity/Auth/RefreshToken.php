8f88e6086f580f60c72508232757be6c6af836b4
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2026-03-18 17:08:11 +0100

    feat(api): extend RefreshToken entity with additional fields
    Replace `Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken` with `Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken` as the base class. Now "future-proofed" as it no longer depends on the deprecated classes directly
    
    Add new properties: `id`, `refreshToken`, `username`, and `valid` to align with database requirements and improve token management flexibility.

--- api/src/Entity/Auth/RefreshToken.php
+++ api/src/Entity/Auth/RefreshToken.php
@@ -3,10 +3,24 @@
 namespace App\Entity\Auth;
 
 use Doctrine\ORM\Mapping as ORM;
-use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshTokenRepository;
+use Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken as BaseRefreshToken;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: RefreshTokenRepository::class)]
 #[ORM\Table(name: 'refresh_tokens', schema: 'auth')]
 class RefreshToken extends BaseRefreshToken
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+
+    #[ORM\Column(name: 'refresh_token', type: 'string', length: 128, unique: true)]
+    protected $refreshToken;
+
+    #[ORM\Column(name: 'username', type: 'string', length: 255)]
+    protected $username;
+
+    #[ORM\Column(name: 'valid', type: 'datetime')]
+    protected $valid;
 }
